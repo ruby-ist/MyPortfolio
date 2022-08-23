540932942ad5b7a35fce835e8f15cc5937692185
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-08-23 20:46:15 +0530

    Initial Commit with Nuxt 3

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+    ssr: false,
+
+    target: 'static',
+
+    app: {
+        head: {
+            title: 'GeoBITs',
+            htmlAttrs: {
+                lang: 'en'
+            },
+            meta: [
+                {charset: 'utf-8'},
+                {name: "viewport", content: "width=device-width, initial-scale=1, user-scalable=0"},
+            ],
+            link: [
+                {
+                    rel: "stylesheet",
+                    type: "text/css",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.css"
+                },
+            ],
+            script: [
+                {src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"},
+                {src: "https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.8/semantic.min.js"},
+                {src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"}
+            ]
+        }
+    },
+
+    modules: [
+        // '@nuxt/http'
+    ],
+
+    buildModules: [
+        // '@nuxtjs/composition-api/module',
+        // '@pinia/nuxt'
+    ],
+
+    // http: {
+    //     proxy: true
+    // },
+    //
+    // server: {
+    //     port: 3000,
+    //     host: '0.0.0.0'
+    // },
+
+    components: true
+})
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 class="ui huge header">Hello World</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped lang="scss">
+div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    h1{
+        color: #41b883 !important;
+    }
+}
+</style>
