5401a5f39e718157f44c2742d7b77d5886e65b0d
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2022-10-24 20:06:15 +0530

    Loader added

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,38 @@
 <template>
-    <div id="main">
-        <NavBar />
-        <Profile />
-        <Skills />
-        <Projects />
-        <ProjectsMobile />
-        <Featured />
-        <Footer />
+    <Loader v-if="loading" />
+    <div v-if="!(loading)" id="main">
+        <NavBar/>
+        <Profile/>
+        <Skills/>
+        <Projects/>
+        <ProjectsMobile/>
+        <Featured/>
+        <Footer/>
     </div>
 </template>
 
 <script lang="ts">
-import { defineNuxtComponent } from '#app'
+import {defineNuxtComponent} from '#app'
+import {gsap} from "gsap"
+
 export default defineNuxtComponent({
-    mounted(){
-        if(screen.width > 576){
-            window.onresize = function(){ location.reload(); }
+    data: () => ({
+        loading: true
+    }),
+    mounted() {
+        setTimeout( () => {
+            gsap.timeline().to('.curtain', {
+                opacity: 0,
+                duration: 0.3
+            });
+        }, 1900);
+        setTimeout(() => {
+            this.loading = false;
+        }, 2100);
+        if (screen.width > 576) {
+            window.onresize = function () {
+                location.reload();
+            }
         }
         document.documentElement.scrollTop = 0;
     }
@@ -24,15 +41,17 @@ export default defineNuxtComponent({
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Signika+Negative:wght@400;500;600&display=swap');
-:root{
+
+:root {
     &::-webkit-scrollbar {
         display: none;
     }
+
     -ms-overflow-style: none;
     scrollbar-width: none;
 }
 
-#main{
+#main {
     background: #F5F6F9;
     font-family: 'Signika Negative', sans-serif;
     color: #393E46;
